3f2f3114962b089e3eca65851693a8dc7e46d193
lucasespinosa28 <52639395+lucasespinosa28@users.noreply.github.com>
2023-11-16 18:55:38 -0300

    removidf

--- packages/frontend/src/stories/templates/select.tsx
+++
@@ -1,8 +0,0 @@
-import { useRef } from "react";
-import contracts from "../../../../contract/address.json";
-import { SelectToken } from "../../compoments/inputs/selectToken";
-
-export default function Template() {
-  const tokenRef = useRef<`0x${string}`>(contracts.token as `0x${string}`);
-  return <SelectToken tokenRef={tokenRef} />;
-}
