8f2670be6ea03e4b23a52cedd065fbc0a442856b
lucasespinosa28 <52639395+lucasespinosa28@users.noreply.github.com>
2023-11-17 12:07:05 -0300

    final part

--- packages/testing/cypress/e2e/createPayment.cy.js
+++ packages/testing/cypress/e2e/createPayment.cy.js
@@ -7,19 +7,23 @@ describe('complete payment spec', () => {
         cy.visit('/')
         cy.wait(2000);
         cy.get("#create").click();
+        cy.screenshot("part1")
         // cy.get('#newList').click()
-        for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 10; index++) {
             cy.get("#addInput").click();
             cy.get("#Amount" + index).clear().type(faker.number.float({ min: 0.5, max: 10, precision: 0.01 }))
             cy.get("#Receiver" + index).type(generateRandomHex())
-            cy.get("#Date" + index).type("2023-11-13")
+            cy.get("#Date" + index).type("2023-11-16")
             if (index == 0) {
                 cy.get("#Date" + index).type("2023-11-20")
             }
         }
+        cy.screenshot("part2")
         cy.scrollTo('bottom')
         cy.get("#create").click();
         cy.scrollTo('bottom')
+        cy.screenshot("part3")
         cy.get("#payId").click();
+        cy.screenshot("part4")
     })
 })
